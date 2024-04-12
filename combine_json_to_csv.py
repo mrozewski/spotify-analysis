80ddb55d2b542665e049d59f891232eabf66fb56
mrozewski <155000607+mrozewski@users.noreply.github.com>
2024-04-12 12:01:53 -0600

    Add files via upload

---
+++ combine_json_to_csv.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 11 17:07:14 2024
+
+@author: michaelrozewski
+"""
+
+import os
+import pandas as pd
+
+path = "/Users/michaelrozewski/Desktop/data"
+
+files = os.listdir(path)
+df_list = []
+
+
+#loop through file list, adding json files as dataframes to df_list
+#ds_store hidden file was messing it up, so excepting that one
+for file in files:
+    try:
+       print('Processing', file)
+       file_path = os.path.join(path, file)
+       df_list.append(pd.read_json(file_path, encoding='utf-8'))
+    except:
+        file.startswith('.')
+
+#now, concatenate the list of dfs together, translate to csv
+df = pd.concat(df_list)
+df.to_csv(os.path.join(path, 'combined_data.csv'))
+
+
